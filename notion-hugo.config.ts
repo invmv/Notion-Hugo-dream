6b1c3a534c575216d874062582d0faf3f06f759f
invmv <invmy@outlook.com>
2024-07-30 19:38:05 +0800

    zoom

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/hugo-dream-e0adbe00c1824d3191debfa7ce2e6ee5',
         pages: [
             // {
             //     page_id: '<page_id>',
